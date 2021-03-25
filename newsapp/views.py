d0fa119f061aef659d9fc7e5fecab3a2c2ec003c
Mohit Kumar Mandhre <mohitmandhre@gmail.com>
2021-03-25 13:08:57 +0530

    2nd one

--- newsapp/views.py
+++ newsapp/views.py
@@ -8,7 +8,7 @@ import requests
 #  renders an html as a response. Firstly we import newsapi from NewsApiClient.
 
 # def index(request) : 
-    # newsapi = NewsApiClient(api_key="785a40ced8ee434fba01719fb47a9be9")
+    # newsapi = NewsApiClient(api_key=key")
     # top = newsapi.get_top_headlines(sourses="techcrunch")
 
     # l = top['articles'] 
@@ -27,8 +27,7 @@ import requests
 
 def index(request) :
 
-    # url = ('https://newsapi.org/v2/everything?q=Apple&from=2021-03-24&sortBy=popularity&apiKey=785a40ced8ee434fba01719fb47a9be9')
-    url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=785a40ced8ee434fba01719fb47a9be9')
+    url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=API KEY')
     response = requests.get(url)
     r = response.json() 
     l = r['articles']
